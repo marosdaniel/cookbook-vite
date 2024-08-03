ce5cc42c7289c97d2812f4b52ee93483b7b34d0c
Daniel Maros <marosdaniel@gmail.com>
2024-08-03 11:12:00 +0200

    shell part 3

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -1,15 +1,27 @@
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { AppShell, Avatar, Burger, Divider, Group, NavLink } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { TProps } from './types';
-import { useTopMenuItems } from './utils';
-import Logo from '../Logo';
 import { useAuthState } from '../../store/Auth';
+import Logo from '../Logo';
+import { useBottomMenuItems, useTopMenuItems } from './utils';
+import { IBottomMenuItem, TProps } from './types';
 
 const Shell = ({ children }: TProps) => {
+  const navigate = useNavigate();
   const { isAuthenticated } = useAuthState();
   const [opened, { toggle }] = useDisclosure();
   const topMenuItems = useTopMenuItems();
+  const bottomMenuItems = useBottomMenuItems();
+
+  const handleClick = (e: React.SyntheticEvent, item: IBottomMenuItem) => {
+    if (item.action) {
+      e.preventDefault();
+      item.action();
+    } else if (item.path) {
+      navigate(item.path);
+    }
+  };
+
   return (
     <AppShell
       header={{ height: 60 }}
@@ -47,6 +59,22 @@ const Shell = ({ children }: TProps) => {
           </Group>
         </AppShell.Section>
         <Divider />
+        <AppShell.Section>
+          <Group gap={0} p={8}>
+            {bottomMenuItems.map(item => {
+              return !item.hidden ? (
+                <NavLink
+                  component={RouterLink}
+                  key={item.key}
+                  to={item.path ?? '/'}
+                  label={item.name}
+                  leftSection={<item.iconComponent />}
+                  onClick={e => handleClick(e, item)}
+                />
+              ) : null;
+            })}
+          </Group>
+        </AppShell.Section>
       </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
