b484c0a3d39a956a0178128e49a8448858f1fa4a
Daniel Maros <marosdaniel@gmail.com>
2024-08-03 09:02:50 +0200

    base?

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ plugins: [['@swc/plugin-styled-components', {}]], devTarget: 'es2022' })],
-  appType: 'mpa',
+  appType: 'spa',
+  base: './',
 });
