63454ed56c83b33c666cef5b9839aa236d1974a1
Daniel Maros <marosdaniel@gmail.com>
2024-08-08 18:04:24 +0200

    fix footer pos

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -133,7 +133,7 @@ const Shell = ({ children }: IProps) => {
       </AppShell.Navbar>
 
       <AppShell.Main className={classNames.main}>{children}</AppShell.Main>
-      <AppShell.Footer w={!isMobile ? `calc(100% - ${APP_SHELL_WIDTH}px)` : '100%'} ml="auto">
+      <AppShell.Footer w={!isMobile ? `calc(100% - ${APP_SHELL_WIDTH}px)` : '100%'} ml="auto" pos="unset">
         <Footer />
       </AppShell.Footer>
     </AppShell>
